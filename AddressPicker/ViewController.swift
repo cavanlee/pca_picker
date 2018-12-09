aa7498a5639c47b91ef252263e2f6c78a951cbb8
cavanlee <1662682515@qq.com>
2018-12-09 11:52:51 +0800

    add result value in view.

--- AddressPicker/ViewController.swift
+++ AddressPicker/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     let picker = AddressPickerView()
     let showBtn = UIButton.init(frame: CGRect(x: 100, y: 150, width: 100, height: 40))
+    let resultLabel = UILabel.init(frame: CGRect(x: 0, y: 400, width: UIScreen.main.bounds.width, height: 30))
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,11 +27,19 @@ class ViewController: UIViewController {
         showBtn.layer.borderWidth = 1
         showBtn.addTarget(self, action: #selector(showPickerAction), for: .touchUpInside)
         
+        
+        resultLabel.font = UIFont.systemFont(ofSize: 14)
+        resultLabel.textColor = UIColor.gray
+        resultLabel.textAlignment = .center
+        view.addSubview(resultLabel)
+        
+        picker.selectValue = { (p, c, a) in
+            self.resultLabel.text = p + c + a
+        }
     }
     
     @objc func showPickerAction() {
         picker.show()
-        view.addSubview(picker)
     }
     /*
     // MARK: - Navigation
